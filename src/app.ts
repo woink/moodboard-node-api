d81cc7a549b4e0338af2a9dbec5a6ea1df7b2ba3
woink <wardprice@gmail.com>
2021-01-16 22:59:04 -0500

    build: :building_construction: added mongoose and connected

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-import express, { Application } from 'express';
-import { routes } from './routes';
-
-export const app: Application = express();
-
-routes(app);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import { app } from './app';
+import express, { Request, Response, Application } from 'express';
+import bodyParser from 'body-parser';
+import { routes } from './routes';
+import connect from './connect';
+import dotenv from 'dotenv';
+
+// ADD MONGODB
 
 const port = 5000;
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to the homepage');
+});
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
+const db = `mongodb+srv://woink:${process.env.MOODBOARD_PASS}@cluster0.oc0mt.mongodb.net/main?retryWrites=true&w=majority`;
+connect({ db });
+routes(app);
