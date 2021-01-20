ecd9eff616470fd1ff116b57823d94fce32f6499
woink <wardprice@gmail.com>
2021-01-19 21:07:55 -0500

    refactor(router): :art:

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response, Application } from 'express';
+import express, { Application } from 'express';
 import bodyParser from 'body-parser';
-import { routes } from './routes';
+
 import connect from './connect';
+import boardRoutes from './routes/boards';
 
 require('dotenv').config();
 
@@ -11,12 +12,14 @@ const app: Application = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to the homepage');
-});
+app.use(boardRoutes);
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Welcome to the homepage');
+// });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
 
 const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_ADDRS}?retryWrites=true&w=majority`;
 connect({ db });
-routes(app);
+// routes(app);
