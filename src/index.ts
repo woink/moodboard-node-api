091ce8ce47e481b6ee1255d2333c2503d2d4fa97
woink <wardprice@gmail.com>
2021-02-04 00:16:50 -0500

    style: :art:

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response, Application } from 'express';
+import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import connect from './connect';
-import routes from './routes';
+import imageRoutes from './routes/imagesRoutes';
+import boardRoutes from './routes/boardsRoutes';
 
 require('dotenv').config();
 
@@ -13,8 +14,13 @@ app
   .use(morgan('dev'))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
+
+  .use('/api/v1/boards', boardRoutes)
+  .use('/api/v1/images', imageRoutes)
+
   .listen(port, () => console.debug(`Server is listening on port ${port}`));
 
-const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_ADDRS}?retryWrites=true&w=majority`;
+// const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_ADDRS}?retryWrites=true&w=majority`;
+
+const db = 'mongodb://localhost:27017/moodboard';
 connect({ db });
-routes({ app });
