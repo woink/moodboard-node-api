0cd077fa98f80a8191340a34966f4ff7f5606c82
woink <wardprice@gmail.com>
2021-02-18 18:44:45 -0500

    refactor(image-upload): :recycle: renamed to localStorage

--- src/services/ImageUpload.ts
+++ src/services/ImageUpload.ts
@@ -1,7 +1,7 @@
 import multer from 'multer';
 import path from 'path';
 
-const storage = multer.diskStorage({
+const localStorage = multer.diskStorage({
   destination: function (req: Express.Request, file, cb) {
     cb(null, './src/public/images');
   },
@@ -19,4 +19,4 @@ const fileFilter = (req: Request, file: any, cb: any) => {
   }
 };
 
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage: localStorage });
