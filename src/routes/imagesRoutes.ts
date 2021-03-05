71102244c23a92eabcd2c4f14a89b8c124d4213d
woink <wardprice@gmail.com>
2021-03-05 00:06:10 -0500

    revert: :fire: remove previous approaches to upload images to cloudinary

--- src/routes/imagesRoutes.ts
+++ src/routes/imagesRoutes.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import imagesController from '../controllers/imagesController';
-import imageUploader from '../services/ImageUpload';
 
 const router = express.Router();
 
 router
   .get('/', imagesController.getImages)
-  .post('/upload', imageUploader.single('image'), imagesController.uploadImage)
+  .post('/upload', imagesController.uploadImage)
   .get('/:id', imagesController.getImage);
 
 export default router;
